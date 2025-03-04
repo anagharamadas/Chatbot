34063391b92e2ea3018ca2301f3774196ef05249
Anagha Ramadas Mulloth <55655339+anagharamadas@users.noreply.github.com>
2025-03-04 12:13:07 +0000

    installing packages

--- api/client.py
+++
@@ -1,8 +0,0 @@
-import requests
-import streamlit as st
-
-def get_ollama_response(input_text):
-    response = requests.post("http://localhost:8000/poem/invoke", 
-    json={"input": {'topic': input_text}})
-
-    return response.json()['output']
